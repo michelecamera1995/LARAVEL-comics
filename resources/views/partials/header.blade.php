9e62cd20ff52bda1ccfdecfc22fd0c48856d9cb7
michele camera <michy.cam95@gmail.com>
2022-05-18 16:50:50 +0200

    modify css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,21 @@
-<div id="header">
-    <div class="header container">
-        <img src="../assets/dc-logo.png" alt="DC-Logo" />
-        <nav>
-            <ul>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
-            </ul>
-        </nav>
+<header>
+    <div id="header">
+        <div class="header container">
+            <img src="../assets/dc-logo.png" alt="DC-Logo" />
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
-</div>
+</header>
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,15 +1,17 @@
-<div id="Main">
-    <div id="jumbo"></div>
-    <div class="Main container">
-        <div class="currentLabel">
-            <h2>CURRENT SERIES</h2>
+<main>
+    <div id="Main">
+        <div id="jumbo"></div>
+        <div class="Main container">
+            <div class="currentLabel">
+                <h2>CURRENT SERIES</h2>
+            </div>
+            @foreach($comics as $comic)
+            <div class="card">
+                <img src="{{ $comic['thumb']}}" />
+                <h2>{{ $comic['title']}}</h2>
+                <p>{{ $comic['price'] }}</p>
+                <p>{{ $comic['type']}}</p>
+            </div>
+            @endforeach
         </div>
-        @foreach($comics as $comic)
-        <div class="card">
-            <img src="{{ $comic['thumb']}}" />
-            <h2>{{ $comic['title']}}</h2>
-            <p>{{ $comic['price'] }}</p>
-            <p>{{ $comic['type']}}</p>
-        </div>
-        @endforeach
-    </div>
+</main>
