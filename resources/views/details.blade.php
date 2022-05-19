389a7bf854702423fa6bb60506beb5324f7cfa60
michele camera <michy.cam95@gmail.com>
2022-05-19 14:38:08 +0200

    created css for details

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 
 <div id="Main">
-    ciao sono details
+    <div id="details">
+        <img id="details-img" src="{{ $comics['thumb']}}" alt="{{ $comics['title']}}">
+        <h1 id="details-title">{{ $comics['title']}}</h1>
+        <p id="details-series">{{ $comics['series']}}</p>
+        <p id="details-description">{{ $comics['description']}}</p>
+        <p id="details-price">{{ $comics['price']}}</p>
+    </div>
 </div>
 
 @endsection
